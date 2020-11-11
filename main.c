016629a7193bdb555c7effe31f7ea9256ac1c785
Özge <ozgekeskin2003@hotmail.com>
2020-11-11 14:25:38 +0300

    deneme

---
+++ main.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+printf("Merhaba yasirmaci bugun seninle bir oyun oynayacagiz\n");
+	printf("Sana bazi sorular soracagim ve verdigin cevaplar dogru iseeee\n");
+	printf("yarismanin sonunda seni bir odul bekleyecek\n");
+	printf("Sen de hazirsan baslayalim.\n");
+	printf("Ýste ilk sorumuz: \n");
+	printf("Annemizin annesinin erkek kardesi bizim neyimiz olur?\n");
+	printf("1-dayimiz\n");
+	printf("2-amcamiz\n");
+	printf("3-teyzemiz\n");
+	
+	int a,b,c,toplam;
+	a=1;
+	b=3;
+	c=4;
+	toplam=a;
+	printf("Cevabini alalim :))\n");
+	scanf("%d",&toplam);
+	if(toplam<=1)
+	{
+		printf("En basit soruyu dogru bildin :)) Tebrikler. Siradaki soru geliyor.:))");
+	}
+	else
+	{
+		printf("ilk soruda elendin hoscakal...\n");
+	}
+		printf("Simdi ikinci sorumuz geliyor.\n");
+	printf("Bu yalnýzca 4 soruluk bir yarisma olacak. Sanirim bunu sana söylememistim.\n");
+	printf("Pas gecme ve joker hakkin yok. Onun icin en iyisini yapmaya calis :))\n");
+	printf("Hazir misin?\n");
+	printf("Bildigimiz gibi bazi aylar 30 bazi aylar 31 gun ceker peki hangi ayda/aylarda 28 gun vardir?\n");
+	printf("1-yedi\n");
+	printf("2-on iki\n");
+	printf("3-bir\n");
+	
+	int d,e,f,toplam2;
+	d=8;
+	e=2;
+	f=5;
+	toplam2=e;
+	printf("Ýyi dusunmelisin :))\n");
+	printf("Simdi cevabini alalim.\n");
+	scanf("%d",&toplam);
+	if(toplam<=2)
+	{
+		printf("Okudugunu anliyorsun.Tebrikler. Yarismanin yarisi bitirdik.:))\n");
+	}
+	else
+	{
+		printf("Soruyu duzgun okumadin... Uzgunum\n");
+	}
+	printf("Geldik ucuncu soruya.\n");
+	printf("Eger bu soruyu da bilirsen sana diger soruda bir surpriz yapacagim.\n");
+	printf("Hazirsan siradaki soruya geciyoruz.\n");
+	printf("Biraz cografya bilgini ölcelim :))\n");
+	printf("Maki daha cok hangi bolgemizde gorulen bir bitki turudur?\n");
+	printf("1-Akdeniz\n");
+	printf("2-Ýc Anadolu\n");
+	printf("3-Marmara");
+	
+	int g,h,j,cevap3;
+	g=1;
+	h=2;
+	j=3;
+	cevap3=g;
+	printf("Unutma ki bu sorudan sonra sana bir iyilik yapacagim :)\n");
+	printf("Simdi cevabini alalim:\n");
+	scanf("%d",&cevap3);
+	if(cevap3<=1)
+	{
+		printf("Tebrik ederim artýk son soruya geldik. Hazir misin?");
+	}
+	else
+	{
+		printf("Surprizimi goremedin :(( Bu basit bir soruydu... Hoscakal :))");
+	}
+	printf("Sana surprizim su:\n");
+	printf("Bu soruda bir ipucun olacak.\n");
+	printf("Beklentini karsilamadi mi? :))\n");
+	printf("Sorumuza gecelim :)\n");
+	printf("Bu bir tarih sorusu yarismaci.Sorunun dogru cevabinda yer alan topluluk zamanýnda:\n");
+	printf("'......... baris cagi' unvaninin sahibi olarak biliniyordu\n");
+	printf("sorumuz geliyor:\n");
+	printf("Museviligi kabul eden ilk turk toplulugu hangisidir?\n");
+	printf("1-Hazarlar\n");
+	printf("2-Uygurlar\n");
+	printf("3-Harzemsahlar\n");
+	
+	int v,y,z,toplm;
+	v=1;
+	y=2;
+	z=3;
+	toplm=v;
+	printf("Cevabini bekliyorum. :))\n");
+	scanf("%d",&toplm);
+	if(toplm<=1)
+	{
+		printf("Tebrik ederim bu son sorumuzdu. odulunu almak icin 'enter'tusuna basmalisin. :))\n");
+		}	
+    else
+    {
+    	printf(":(( Tarih bilgin zayifmis. hoscakal maalesef yarismayi son soruda kaybettin :)\n)");
+	}
+
+	return 0;
+}
